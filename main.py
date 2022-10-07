877840bc7e7a8518115f0c7d1d7119939f9821dc
Andre <raffa.rolon3@gmail.com>
2022-10-06 20:15:11 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,26 @@
+music_class = {
+    "class": "drums",
+    "name": "Emma",
+    "schedule": ("wednesday", "7pm"),
+    "hours": "2"
+}
+
+my_appointment = {
+    "date": ("November", "10th"),
+    "hour": "9am",
+    "site": "Jardin_Principal",
+    }
+
+concert = {
+    "band": "Muse",
+    "genre": "alternative",
+    "date": "September_28th",
+    "site": "Sunrise_Theater",
+    "hour": "9pm"
+
+}
+def obtener_nombre():
+nombre_usuario = input("ingrese su nombre por favor:")
+print("Hola", nombre_usuario)
+
+def obtener
